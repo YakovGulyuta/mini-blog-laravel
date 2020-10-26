f8ba46d7affdbe3a1ddd403e31553b9c92239ad8
Yakov <yakovgulyuta@yandex.ru>
2020-10-26 23:32:45 +0300

    add Admin LTE views and assets

---
+++ resources/views/layouts/admin/default.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+{{--HEAD THIS--}}
+@include('admin.parts.head')
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+
+{{--THIS CONTENT--}}
+@yield('content')
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('js/jquery.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('js/adminlte.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('js/demo.js')}}"></script>
+</body>
+</html>
