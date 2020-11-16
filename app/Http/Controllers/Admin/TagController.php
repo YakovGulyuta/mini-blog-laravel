e321e8ce02ad88fb7d5c57d8c2091e0743dcf8c4
Yakov <yakovgulyuta@yandex.ru>
2020-11-16 14:55:02 +0300

    add Teg resource

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $tags = [
+            ['id' => '1',
+                'title' => 'Тег',
+                'slug' => 'tag'
+            ]
+        ];
+
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $tag = [
+            'id' => '1',
+                'title' => 'Тег',
+                'slug' => 'tag'
+
+        ];
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //return redirect()->route('tags.index')->with('success', 'Изменения сохранены');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/layouts/admin/default.blade.php
+++ resources/views/layouts/admin/default.blade.php
@@ -8,10 +8,35 @@
 
 @include('parts.admin.navbar.navbar')
 @include('parts.admin.aside.aside')
+
 {{--THIS CONTENT--}}
     <div class="content-wrapper" style="min-height: 855px;">
+
+{{--        <div class="container mt-2">--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-12">--}}
+{{--                    @if ($errors->any())--}}
+{{--                        <div class="alert alert-danger">--}}
+{{--                            <ul class="list-unstyled">--}}
+{{--                                @foreach ($errors->all() as $error)--}}
+{{--                                    <li>{{ $error }}</li>--}}
+{{--                                @endforeach--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    @endif--}}
+
+{{--                    @if (session()->has('success'))--}}
+{{--                        <div class="alert alert-success">--}}
+{{--                            {{ session('success') }}--}}
+{{--                        </div>--}}
+{{--                    @endif--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
 @yield('content')
     </div>
+
 <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
 </aside>
