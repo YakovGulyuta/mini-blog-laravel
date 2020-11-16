b597208148940eaa247b65422a837d9449e92eaa
Yakov <yakovgulyuta@yandex.ru>
2020-11-16 16:22:12 +0300

    update migration and add Artcile rosource

--- database/migrations/2020_11_16_091643_create_article_tag_table.php
+++ database/migrations/2020_11_16_091643_create_article_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostTagTable extends Migration
+class CreateArticleTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePostTagTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_tag', function (Blueprint $table) {
+        Schema::create('article_tag', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('article_id')->unsigned();
             $table->integer('tag_id')->unsigned();
-            $table->integer('post_id')->unsigned();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreatePostTagTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('article_tag');
     }
 }
